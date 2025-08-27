5af17aeefb2e561417c3e73b8cc5fbe3c310eb86
hariharan s <hariharans.developer@gmail.com>
2025-08-27 15:52:43 +0530

    getPlan endpoint

--- controller/subscription.js
+++ controller/subscription.js
@@ -108,7 +108,7 @@ export default class SubscriptionController {
         return res.status(201).json(createApiResponse({}, 201));
       }
       response = response.map((plan) => plan.toJSON());
-      return res.status(201).json(createApiResponse({ response }, 201));
+      return res.status(201).json(createApiResponse(response, 201));
     } catch (error) {
       console.log(error);
       return res
